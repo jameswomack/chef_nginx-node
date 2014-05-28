6f385b18d5c6e723f73629a501cff9605141809e
James Womack <james.womack@am.sony.com>
2014-05-28 05:02:50 +0000

    Add upstart

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 rm -rf /chef
 cp -R $(pwd) /chef
+
+sudo touch /var/log/node_3001.log
+sudo touch /var/log/node_3002.log
+sudo chmod 777 /var/log/node_3001.log
+sudo chmod 777 /var/log/node_3002.log
+
 cd /chef
 librarian-chef install
 chef-solo -c solo.rb
+
+start uxtcExpress
